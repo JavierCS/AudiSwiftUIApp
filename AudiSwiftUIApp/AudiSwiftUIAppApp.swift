869405888e05d99738440e000adbfca3b111b973
Javier Cruz Santiago <javiercruzsantiago@icloud.com>
2022-11-28 21:52:34 -0600

    First Commit

---
+++ AudiSwiftUIApp/AudiSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudiSwiftUIAppApp.swift
+//  AudiSwiftUIApp
+//
+//  Created by jcruzsa on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AudiSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
